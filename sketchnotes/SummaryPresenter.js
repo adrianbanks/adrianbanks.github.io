48c0d124170d7e2718abe6ff5e0501d3bc85e990
Adrian Banks <adrian.banks@red-gate.com>
2023-10-29 23:14:46 +0000

    Simplify some methods

--- sketchnotes/SummaryPresenter.js
+++ sketchnotes/SummaryPresenter.js
@@ -14,21 +14,13 @@ export class SummaryPresenter {
         this.#summary = new GroupedSummary(sketchnotes);
     }
 
-    addConferences = (list) => {
-        this.#summary.byConference().forEach(conference => this.#addListItem(list, 'conference', conference));
-    }
+    addConferences = (list) => this.#summary.byConference().forEach(conference => this.#addListItem(list, 'conference', conference));
 
-    addEvents = (list) => {
-        this.#summary.byEvent().forEach(event => this.#addListItem(list, 'event', event));
-    }
+    addEvents = (list) => this.#summary.byEvent().forEach(event => this.#addListItem(list, 'event', event));
 
-    addSpeakers = (list) => {
-        this.#summary.bySpeaker().forEach(speaker => this.#addListItem(list, 'speaker', speaker));
-    }
+    addSpeakers = (list) => this.#summary.bySpeaker().forEach(speaker => this.#addListItem(list, 'speaker', speaker));
 
-    addTags = (list) => {
-        this.#summary.byTag().forEach(tag => this.#addListItem(list, 'tag', tag));
-    }
+    addTags = (list) => this.#summary.byTag().forEach(tag => this.#addListItem(list, 'tag', tag));
     
     #addListItem = (list, type, item) => {
         let html = `<li><a class="modal-link" link-type="${type}" link-value="${item.name}" href="#" rel="modal:close">${item.name}</a>`;
