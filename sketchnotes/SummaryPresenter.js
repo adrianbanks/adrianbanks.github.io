3cd94a0290fc646b89408f3265630badd7bcdc78
Adrian Banks <adrian.banks@red-gate.com>
2026-02-01 18:09:50 +0000

    Use dynamic columns for summary lists

--- sketchnotes/SummaryPresenter.js
+++ sketchnotes/SummaryPresenter.js
@@ -14,14 +14,26 @@ export class SummaryPresenter {
         this.#summary = new GroupedSummary(sketchnotes);
     }
 
-    addConferences = (list) => this.#summary.byConference().forEach(conference => this.#addListItem(list, 'conference', conference));
+    addConferences = (list) => {
+        this.#summary.byConference().forEach(conference => this.#addListItem(list, 'conference', conference));
+        this.#configureColumns(list);
+    }
 
-    addEvents = (list) => this.#summary.byEvent().forEach(event => this.#addListItem(list, 'event', event));
+    addEvents = (list) => {
+        this.#summary.byEvent().forEach(event => this.#addListItem(list, 'event', event));
+        this.#configureColumns(list);
+    }
 
-    addSpeakers = (list) => this.#summary.bySpeaker().forEach(speaker => this.#addListItem(list, 'speaker', speaker));
+    addSpeakers = (list) => {
+        this.#summary.bySpeaker().forEach(speaker => this.#addListItem(list, 'speaker', speaker));
+        this.#configureColumns(list);
+    }
 
-    addTags = (list) => this.#summary.byTag().forEach(tag => this.#addListItem(list, 'tag', tag));
-    
+    addTags = (list) => {
+        this.#summary.byTag().forEach(tag => this.#addListItem(list, 'tag', tag));
+        this.#configureColumns(list);
+    }
+
     #addListItem = (list, type, item) => {
         let html = `<li><a class="modal-link" link-type="${type}" link-value="${item.name}" href="#" rel="modal:close">${item.name}</a>`;
 
@@ -31,5 +43,19 @@ export class SummaryPresenter {
 
         html += '</li>';
         list.append(html);
+    }
+
+    #configureColumns = (list) => {
+        const itemCount = list.children('li').length;
+        const columnCount = itemCount < 15
+            ? 1
+            : itemCount < 50
+                ? 2
+                : itemCount < 100
+                    ? 3
+                    : 4;
+        console.log(`Setting column count to ${columnCount} for ${itemCount} items`);
+        console.log(list);
+        list.css('column-count', columnCount);
     }
 }
